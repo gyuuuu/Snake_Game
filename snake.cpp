daf2838a43f7a7e3ab0d73a997a9ab76d8ccfc27
BeautterLife <pgh9801@gmail.com>
2021-06-21 00:42:27 +0900

    Add files via upload

--- snake.cpp
+++ snake.cpp
@@ -1,14 +1,6 @@
 #include "snake.h"
 
-Body::Body()
-{
-    Body(0, 0);
-}
-Body::Body(int x_pos, int y_pos)
-{
-    x = x_pos;
-    y = y_pos;
-}
+
 Snake::Snake()
 {
 
@@ -19,13 +11,15 @@ Snake::Snake()
     cbreak();
     nodelay(stdscr, true);
     keypad(stdscr, true);
-    srand(time(NULL));
+    srand((unsigned int)time(NULL));
 
     y = 10;
     x = 20;
 
     direction = 'l';
     score = 0;
+    gateCnt=0;
+    mvCnt=0;
     eaten_fruit = 0, eaten_poison = 0;
     Check_body = ' ', Check_fruit = ' ', Check_poison = ' ', Check_Gate = ' ';
     Check_Complete = false;
@@ -33,12 +27,12 @@ Snake::Snake()
     Check_time = 0;
 
     snake.clear();
-    // creating snake
+
     for (int i = 0; i < 3; i++)
     {
-        Body Body;
-        Body = {y, x};
-        snake.push_back(Body);
+        Body body;
+        body = {y, x};
+        snake.push_back(body);
     }
 }
 Snake::~Snake()
@@ -46,6 +40,16 @@ Snake::~Snake()
     snake.clear();
     nodelay(stdscr, true);
     endwin();
+}
+Body::Body()
+{
+    Body(0, 0);
+}
+
+Body::Body(int yPos, int xPos)
+{
+    y = yPos;
+    x = xPos;
 }
 void Snake::startGame()
 {
@@ -55,7 +59,7 @@ void Snake::startGame()
     createWindows(3);
     createFruit();
     createPoison();
-    createGate();
+   
 
     init_pair(2, COLOR_RED, COLOR_RED);
     init_pair(3, COLOR_GREEN, COLOR_GREEN);
@@ -76,45 +80,330 @@ void Snake::startGame()
 
     gameOver();
 }
-// 전체적인 스네이크 표현 및 조작 함수.
+
+
 bool Snake::moveSnake()
 {
-    int c;
-    c = getch();
-    // 방향키 이벤트 처리 진행방향과 반대방향일시 return 0;
-    switch (c)
+    if (!getKeyboard(getch()))
+       return false;
+    setDir();
+    
+    if (checkCollision() == 0 || snake.size() == 2)
+    {
+        return 0;
+    }
+
+
+    if (snake.size() >= 5)
+        Check_body = 'v'; 
+    if (gateCnt > 0) Check_Gate = 'v';
+    if (mvCnt == snake.size())
+    {
+        DeleteGate();
+    }
+
+    snake.insert(snake.begin(),{y, x});
+    mvaddch(snake.back().y, snake.back().x, ' ');
+    snake.pop_back();
+
+
+    vector<Body>::iterator it;
+    for (it = snake.begin(); it != snake.end(); it++)
+    {
+
+        if (it == snake.begin())
+            mvaddch(it->y, it->x, '#' | COLOR_PAIR(4));
+        else
+            mvaddch(it->y, it->x, 'O' | COLOR_PAIR(5));
+    }
+    if (CheckMission()) return false;
+    setSpeed();
+    if (Check_time==10) createGate();
+    
+    Checking(Check_speed,Check_time);
+    Check_time++;
+    return true;
+
+}
+
+int Snake::checkCollision()
+{
+
+    vector<Body>::iterator it;
+    for (it = snake.begin(); it != snake.end(); it++)
+    {
+
+        if (it->x == fruit.x && it->y == fruit.y)
+        {
+            Body tail;
+            tail.y = snake.back().y;
+            tail.x = snake.back().x;
+            snake.push_back(tail);
+            createFruit();
+            score += 10;
+            eaten_fruit++;
+            if (eaten_fruit == 2)
+                Check_fruit = 'v';
+            Renewall();
+            return 1;
+        }
+
+        if (it->x == poison.x && it->y == poison.y)
+        {
+            Body tail;
+            tail.y = snake.back().y;
+            tail.x = snake.back().x;
+            snake.pop_back();
+            mvaddch(tail.y, tail.x, ' ');
+            createPoison();
+            score -= 10;
+            eaten_poison++;
+            if (eaten_poison >= 1)
+                Check_poison = 'v';
+            Renewall();
+            return 1;
+        }
+
+        // 반대 방향으로 전환한 경우
+        if (it->x == x && it->y == y)
+        {
+            return 0;
+        }
+
+        enterGate();
+        
+        if (it->x == (begX + sizeX) - 1 || it->x == begX || it->y == (begY + sizeY) - 1 || it->y == begY)
+        {
+            // Gate인 경우
+            if (it->x == Gate1.x || it->x == Gate2.x)
+            {
+                mvCnt++;
+                
+                return 1;
+            }
+            else
+                return 0;
+        }
+    }
+}
+void Snake::createFruit()
+{
+    fruit.y = rand() % (HEIGHT - 2) + 2;
+    fruit.x = rand() % (WIDTH - 2) + 2;
+    mvaddch(fruit.y, fruit.x, '*' | COLOR_PAIR(3));
+}
+void Snake::DeleteFruit()
+{
+    mvaddch(fruit.y, fruit.x, ' ');
+}
+void Snake::createPoison()
+{
+    do{
+        poison.y = rand() % (HEIGHT - 2) + 2;
+        poison.x = rand() % (WIDTH - 2) + 2;
+    } while(fruit.x == poison.x || fruit.y == poison.y);
+    mvaddch(poison.y, poison.x, 'X' | COLOR_PAIR(2));
+}
+void Snake::DeletePoison()
+{
+    mvaddch(poison.y, poison.x, ' ');
+}
+void Snake::createGate()
+{
+    gateFlag=false;
+    do{
+        Gate1.x = rand() % (WIDTH-2)+2;
+        Gate1.y = rand() % (HEIGHT-2)+2 ;
+    }while((Gate1.x==poison.x || Gate1.y == poison.y));
+
+    do{
+        Gate2.x = rand() % (WIDTH-2)+2;
+        Gate2.y = rand() % (HEIGHT-2)+2;
+    }while((Gate1.x==poison.x || Gate1.y == poison.y || Gate1.x==poison.x || Gate2.y == poison.y));
+
+    mvaddch(Gate1.y, Gate1.x, 'q' | COLOR_PAIR(6));
+    mvaddch(Gate2.y, Gate2.x, 'w' | COLOR_PAIR(6));
+}
+
+void Snake::DeleteGate()
+{
+    mvaddch(Gate1.y, Gate1.x, ' ');
+    mvaddch(Gate2.y, Gate2.x, ' ');
+}
+
+void Snake::Checking(int check_speed, int Check_time)
+{
+    if (Check_time == 30) Renewall();
+    else if (Check_speed == 2 && Check_time == 43) Renewall();
+    else if (Check_speed == 3 && Check_time == 55) Renewall();
+    else if (Check_speed == 4 && Check_time == 66) Renewall();
+}
+void Snake::Renewall()
+{
+    DeleteFruit();
+    DeletePoison();
+    DeleteGate();
+    createFruit();
+    createPoison();
+    Check_time=0;
+}
+
+void Snake::scoreBoards()
+{
+    mvwprintw(scorewin, 2, 2, "Score Board");
+    mvwprintw(scorewin, 3, 2, "%d / %d ", snake.size(), Max_length);
+    mvwprintw(scorewin, 4, 2, "+ : %d", eaten_fruit);
+    mvwprintw(scorewin, 5, 2, "- : %d", eaten_poison);
+    mvwprintw(scorewin, 6, 2, "speed : %d", Check_speed);
+    mvwprintw(scorewin, 7, 2, "g : %d", gateCnt);
+}
+void Snake::missionBoards()
+{
+    mvwprintw(missionwin, 2, 2, "Mission");
+    mvwprintw(missionwin, 3, 2, "B : 5(%c)", Check_body);
+    mvwprintw(missionwin, 4, 2, "+ : 2(%c)", Check_fruit);
+    mvwprintw(missionwin, 5, 2, "- : 1(%c)", Check_poison);
+    mvwprintw(missionwin, 6, 2, "G : 1(%c)", Check_Gate);
+
+    if (snake.size() < 5 )
+        Check_body = ' ';
+    if (Check_body == 'v' && Check_fruit == 'v' && Check_poison == 'v' && Check_Gate == 'v')
+    {
+        Check_Complete = true;
+    }
+}
+
+void Snake::createWindows(int windowNumber = 3)
+{
+    switch (windowNumber)
+    {
+    case 1: 
+    {
+        gamewin = newwin(HEIGHT, WIDTH, 1, 1);
+        refresh();
+        box(gamewin, '*', '*');
+        wrefresh(gamewin);
+        getmaxyx(gamewin, sizeY, sizeX);
+        getbegyx(gamewin, begY, begX);
+    }
+    break;
+
+    case 2: 
+    {
+        scorewin = newwin(10, 20, 2, 45);
+        refresh();
+        box(scorewin, 0, 0);
+        wrefresh(scorewin);
+        break;
+    }
+    case 3:
+    {
+        missionwin = newwin(10, 20, 12, 45);
+        refresh();
+        box(missionwin, 0, 0);
+        wrefresh(missionwin);
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+bool Snake::CheckMission()
+{
+    if (Check_body =='v' && Check_fruit == 'v' && Check_poison &&Check_Gate=='v')
+        return missionComplete();
+    return false;
+}
+
+void Snake::gameOver()
+{
+    mvwprintw(gamewin, sizeY / 2, sizeX / 2 - 8, "GAME IS OVER!!!");
+    wrefresh(gamewin);
+    usleep(2000000);
+
+    delwin(gamewin);
+    delwin(scorewin);
+    delwin(missionwin);
+    endwin();
+}
+
+bool Snake::missionComplete()
+{
+    mvwprintw(gamewin, sizeY / 2, sizeX / 2 - 8, "MISSION COMPLETE");
+    wrefresh(gamewin);
+    usleep(2000000);
+
+    delwin(gamewin);
+    delwin(scorewin);
+    delwin(missionwin);
+    endwin();
+    return true;
+}
+
+bool Snake::getKeyboard(int ch)
+{
+    switch (ch)
     {
     case KEY_UP:
         if (direction != 'd')
             direction = 'u';
         else
-            return 0;
+            return false;
         break;
     case KEY_DOWN:
         if (direction != 'u')
             direction = 'd';
         else
-            return 0;
+            return false;
         break;
     case KEY_LEFT:
         if (direction != 'r')
             direction = 'l';
         else
-            return 0;
+            return false;
         break;
     case KEY_RIGHT:
         if (direction != 'l')
             direction = 'r';
         else
-            return 0;
+            return false;
         break;
     case 'q':
-        return 0;
-        break; // meanless
+        return false;
+        break; 
     default:
         break;
     }
+    return true;
+}
 
+void Snake::setSpeed()
+{
+    if (snake.size() < 5 || eaten_fruit < 1)
+    {
+        Check_speed=1;
+        usleep(SPEED);
+    }
+    else if (snake.size() <7 || eaten_fruit<2)
+    {
+        Check_speed=2;
+        usleep(SPEED-15000);
+    }
+    else if (snake.size() < 9 || eaten_fruit <3 )
+    {
+        Check_speed=3;
+        usleep(SPEED-25000);
+    }
+    else
+    {
+        Check_speed=4;
+        usleep(SPEED-35000);
+    }
+}
+
+void Snake::setDir()
+{
     switch (direction)
     {
     case 'u':
@@ -130,307 +419,68 @@ bool Snake::moveSnake()
         x++;
         break;
     }
-    // 벽과 충돌했을 경우와 size가 2 이하가 됬을 경우 게임종료.
-    if (checkCollision() == 0 || snake.size() == 2)
-    {
-        return 0;
-    }
-    // Gate 1을 입구로 들어갔을 경우 방향, 좌표 설정.
+}
+
+void Snake::enterGate()
+{
+
     if (y == Gate1.y && x == Gate1.x)
     {
-
-        GateFlag = 1;
-        if (Gate2.x == 1)
+        gateCnt++;
+        gateFlag = true;
+        if (Gate2.x == 1 || Gate2.x == WIDTH) 
         {
-            direction = 'r';
-            x = Gate2.x + 1;
+            direction = Gate2.x == 1? 'r' : 'l';
+            x = Gate2.x ==1 ? 2 : WIDTH-1;
+            y = Gate2.y;
+        }
+        else if(Gate2.y == 1 || Gate2.y == HEIGHT)
+        {
+            direction = Gate2.y == 1? 'd' : 'u';
+            y = Gate2.y ==1 ? 2 : WIDTH-1;
+            x = Gate2.x;
+        }
+        else if (direction=='l'|| direction=='r')
+        {
+            direction = direction == 'l'? 'r' : 'l';
+            x = Gate2.x + (direction=='l'? -1 : 1);
             y = Gate2.y;
         }
         else
         {
-            direction = 'l';
-            x = Gate2.x - 1;
-            y = Gate2.y;
+            direction = direction == 'u'? 'd' : 'u';
+            y = Gate2.y + (direction=='u'? -1 : 1);
+            x = Gate2.x;
         }
     }
-    // Gate 2을 입구로 들어갔을 때
-    else if (y == Gate2.y && x == Gate2.x)
+
+    if (y == Gate2.y && x == Gate2.x)
     {
-        GateFlag = 1;
-        if (Gate1.y == 1)
+        gateCnt++;
+        gateFlag = true;
+        if (Gate1.x == 1 || Gate1.x == WIDTH)
         {
-            direction = 'd';
+            direction = Gate1.x == 1? 'r' : 'l';
+            x = Gate1.x ==1 ? 2 : WIDTH-1;
+            y = Gate1.y;
+        }
+        else if(Gate1.y == 1 || Gate1.y == HEIGHT)
+        {
+            direction = Gate1.y == 1? 'd' : 'u';
+            y = Gate1.y ==1 ? 2 : WIDTH-1;
             x = Gate1.x;
-            y = Gate1.y + 1;
+        }
+        else if (direction=='l'|| direction=='r')
+        {
+            direction = direction == 'l'? 'r' : 'l';
+            x = Gate1.x + (direction=='l'? -1 : 1);
+            y = Gate1.y;
         }
         else
         {
-            direction = 'u';
+            direction = direction == 'u'? 'd' : 'u';
+            y = Gate1.y + (direction=='u'? -1 : 1);
             x = Gate1.x;
-            y = Gate1.y - 1;
         }
     }
-    // size 10이면 미션 보드에 있는 사이즈 미션 체크표시
-    if (snake.size() == 10)
-        Check_body = 'v'; 
-    // 게이트 들어갔을 때 Flag 1로하여서 게이트 표현하는데 이용하였다.
-    if (GateFlag == 1)
-    {
-        Gatecnt++;
-    }
-    // 스네이크가 게이트가 다 나왔을 경우 갱신.
-    if (Gatecnt == snake.size())
-    {
-        mvaddch(Gate2.y, Gate2.x, '*');
-        mvaddch(Gate1.y, Gate1.x, '*');
-        Check_Gate = 'v';
-        createGate();
-    }
-    // 실시간 갱신.
-    snake.push_front({y, x});
-    mvaddch(snake.back().y, snake.back().x, ' ');
-    snake.pop_back();
-
-    // 반복자를 사용해 스네이크 본체 화면에 표시함.
-    list<Body>::iterator i;
-    for (i = snake.begin(); i != snake.end(); i++)
-    {
-
-        if (i == snake.begin())
-            mvaddch(i->y, i->x, '#' | COLOR_PAIR(4));
-        else
-            mvaddch(i->y, i->x, 'O' | COLOR_PAIR(5));
-    }
-    // 사이즈에 따른 레벨, 속도 처리
-    if (snake.size() < 5)
-    {
-        Check_speed=1;
-        usleep(SPEED);
-    }
-    else if (snake.size() == 5 || snake.size() == 6)
-    {
-        Check_speed=2;
-        usleep(180000);
-    }
-    else if (snake.size() == 7 || snake.size() == 8)
-    {
-        Check_speed=3;
-        usleep(130000);
-    }
-    else
-    {
-        Check_speed=4;
-        usleep(90000);
-    }
-
-    Checking(Check_speed,Check_time);
-    Check_time++;
-    return 1;
-}
-// 충돌 경우 정리한 함수
-int Snake::checkCollision()
-{
-    // 반복자 사용해서
-    list<Body>::iterator i;
-    for (i = snake.begin(); i != snake.end(); i++)
-    {
-        // Growth 만났을 때
-        if (i->x == meat.x && i->y == meat.y)
-        {
-            Body tail;
-            tail.y = snake.back().y;
-            tail.x = snake.back().x;
-            snake.push_back(tail);
-            createFruit();
-            score += 10;
-            eaten_fruit++;
-            if (eaten_fruit == 5)
-                Check_fruit = 'v';
-            return 1;
-        }
-        // Poison 만났을 때
-        if (i->x == poison.x && i->y == poison.y)
-        {
-            Body tail;
-            tail.y = snake.back().y;
-            tail.x = snake.back().x;
-            snake.pop_back();
-            mvaddch(tail.y, tail.x, ' ');
-            createPoison();
-            score -= 10;
-            eaten_poison++;
-            if (eaten_poison == 2)
-                Check_poison = 'v';
-            return 1;
-        }
-
-        // 꼬리 물기 했을 경우
-        if (i->x == x && i->y == y)
-        {
-            return 0;
-        }
-
-        // 벽과 충돌했을 경우
-        if (i->x == (begX + sizeX) - 1 || i->x == begX || i->y == (begY + sizeY) - 1 || i->y == begY)
-        {
-            // Gate 예외 처리
-            if (i->x == Gate1.x || i->x == Gate2.x)
-                return 1;
-            else
-                return 0;
-        }
-    }
-}
-// Growth 생성
-void Snake::createFruit()
-{
-    meat.y = rand() % (HEIGHT - 2) + 2;
-    meat.x = rand() % (WIDTH - 2) + 2;
-    mvaddch(meat.y, meat.x, '*' | COLOR_PAIR(3));
-}
-// Growth 제거
-void Snake::DeleteFruit()
-{
-    mvaddch(meat.y, meat.x, ' ');
-}
-// Poison 생성
-void Snake::createPoison()
-{
-    poison.y = rand() % (HEIGHT - 2) + 2;
-    poison.x = rand() % (WIDTH - 2) + 2;
-    mvaddch(poison.y, poison.x, 'X' | COLOR_PAIR(2));
-}
-// Poison 제거
-void Snake::DeletePoison()
-{
-    mvaddch(poison.y, poison.x, ' ');
-}
-// Gate 생성
-void Snake::createGate()
-{
-    GateFlag = 0;
-    Gatecnt = 0;
-    Gate1.y = rand() % 11;
-
-    if (Gate1.y < 5)
-        Gate1.y = 1;
-    else
-        Gate1.y = HEIGHT;
-
-    Gate1.x = (rand() % (WIDTH - 2)) + 2;
-    Gate2.y = (rand() % (HEIGHT - 2)) + 2;
-
-    Gate2.x = rand() % 11;
-    if (Gate2.x < 5)
-        Gate2.x = 1;
-    else
-        Gate2.x = WIDTH;
-    mvaddch(Gate1.y, Gate1.x, 'q' | COLOR_PAIR(6));
-    mvaddch(Gate2.y, Gate2.x, 'w' | COLOR_PAIR(6));
-}
-// Gate 제거
-void Snake::DeleteGate()
-{
-    mvaddch(Gate1.y, Gate1.x, '*');
-    mvaddch(Gate2.y, Gate2.x, '*');
-}
-// 시간에 따른 아이템, 게이트 위치 바꿔주는 함수.
-void Snake::Checking(int check_speed, int Check_time)
-{
-    if (Check_speed == 1 && Check_time == 23) Renewall();
-    else if (Check_speed == 2 && Check_time == 37) Renewall();
-    else if (Check_speed == 3 && Check_time == 50) Renewall();
-    else if (Check_speed == 4 && Check_time == 60) Renewall();
-}
-// 위의 함수의 재사용하기 위해 만든 함수.
-void Snake::Renewall()
-{
-    DeleteFruit();
-    DeletePoison();
-    DeleteGate();
-    createFruit();
-    createGate();
-    createPoison();
-    Check_time=0;
-}
-// 점수 표시하는 윈도우 창.
-void Snake::scoreBoards()
-{
-    mvwprintw(scorewin, 2, 2, "Score Board");
-    mvwprintw(scorewin, 3, 2, "%d / %d ", snake.size(), Max_length);
-    mvwprintw(scorewin, 4, 2, "+ : %d", eaten_fruit);
-    mvwprintw(scorewin, 5, 2, "- : %d", eaten_poison);
-}
-// 미션체크하는 윈도우 창.
-void Snake::missionBoards()
-{
-    mvwprintw(missionwin, 2, 2, "Mission");
-    mvwprintw(missionwin, 3, 2, "B : 10(%c)", Check_body);
-    mvwprintw(missionwin, 4, 2, "+ : 5(%c)", Check_fruit);
-    mvwprintw(missionwin, 5, 2, "- : 2(%c)", Check_poison);
-    mvwprintw(missionwin, 6, 2, "G : 1(%c)", Check_Gate);
-
-    if (snake.size() != 10)
-        Check_body = ' ';
-    if (Check_body == 'v' && Check_fruit == 'v' && Check_poison == 'v' && Check_Gate == 'v')
-    {
-        Check_Complete = true;
-    }
-}
-// 윈도우 세개 별로 따로 설정하는 함수.
-void Snake::createWindows(int windowNumber = 3)
-{
-    switch (windowNumber)
-    {
-    case 1: // 본 게임 창
-    {
-        gamewin = newwin(HEIGHT, WIDTH, 1, 1);
-        refresh();
-        box(gamewin, '*', '*');
-        wrefresh(gamewin);
-        getmaxyx(gamewin, sizeY, sizeX);
-        getbegyx(gamewin, begY, begX);
-    }
-    break;
-    case 2: // 점수 창
-    {
-        scorewin = newwin(10, 20, 2, 45);
-        refresh();
-        box(scorewin, 0, 0);
-        wrefresh(scorewin);
-        break;
-    }
-    case 3: // 미션 창
-    {
-        missionwin = newwin(10, 20, 12, 45);
-        refresh();
-        box(missionwin, 0, 0);
-        wrefresh(missionwin);
-        break;
-    }
-    default:
-        break;
-    }
-}
-// 게임 종료 표시하는 함수
-void Snake::gameOver()
-{
-    mvwprintw(gamewin, sizeY / 2, sizeX / 2 - 8, "GAME IS OVER!!!");
-    wrefresh(gamewin);
-    usleep(2000000);
-
-    delwin(gamewin);
-    delwin(scorewin);
-    endwin();
-}
-// 게임 완료 표시하는 함수
-void Snake::missionComplete()
-{
-    mvwprintw(gamewin, sizeY / 2, sizeX / 2 - 8, "MISSION COMPLETE");
-    wrefresh(gamewin);
-    usleep(2000000);
-
-    delwin(gamewin);
-    delwin(scorewin);
-    endwin();
 }
